9fff355af579ffc09f1c6ba8778ed551ae086120
JoyKgomo <ST10370660@imconnect.edu.za>
2024-11-19 15:18:50 +0200

    Views
    Added the views for the lecturer, HR, Academic Manager and program coordinator

---
+++ POEPART3CMCS/POEPART3CMCS/Controllers/LecturerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace POEPART3CMCS.Controllers
+{
+    public class LecturerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
